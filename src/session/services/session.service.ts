a82c3a0962b8d09db0b99eb746d5dac9e3fe6ae7
caique.moraes <ike.moraes93@gmail.com>
2023-06-14 20:28:41 -0300

    refactor: update SessionService

--- src/session/services/session.service.ts
+++ src/session/services/session.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@nestjs/common'
-import { OrgService } from '../../org/services/org.service'
 import { AuthService } from '../../auth/services/auth.service'
 import { CredentialDTO } from '../dto/credential.dto'
 
 @Injectable()
 export class SessionService {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly orgService: OrgService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   public async create(credential: CredentialDTO): Promise<string> {
     return this.authService.authenticate(credential)
